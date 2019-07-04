4025fe5dbb1ad8aacb39dbb9b8433a184a71b120
megan schulte <megan.schulte.pdx@gmail.com>
2019-07-04 10:40:54 -0700

    add beer component

---
+++ src/components/Beer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Beer(props){
+  return (
+    <div>
+      <style jsx>{`
+        div{
+          background-color: #04b3b3;
+          font-family: sans-serif;
+          margin-left: 100px;
+          margin-right: 100px;
+        }
+        `}</style>
+      <h1 id="beer-name">{props.name}</h1>
+      <hr/>
+      <h3 id="brand">Brewed by:  {props.brand}</h3>
+      <h3 id="price">${props.price} per pint</h3>
+      <h3 id="abv">ABV:  {props.abv}%</h3>
+    </div>
+  );
+}
+
+Beer.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  abv: PropTypes.number
+};
+
+export default Beer;
--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -7,37 +7,37 @@ let masterTicketList = [
     name: 'Lambert Lager',
     brand: 'Highlander Taigh-grùdaidh',
     price: '5',
-    abv: '5%'
+    abv: '5'
   },
   {
     name: 'MacLeaod Märzen',
     brand: 'Highlander Taigh-grùdaidh',
     price: '5',
-    abv: '5.5%'
+    abv: '5.5'
   },
   {
     name: 'Immortal IPA',
     brand: 'Highlander Taigh-grùdaidh',
     price: '6',
-    abv: '6%'
+    abv: '6'
   },
   {
     name: 'Ramirez Red',
     brand: 'Highlander Taigh-grùdaidh',
     price: '5',
-    abv: '5%'
+    abv: '5'
   }
   {
     name: 'Kurgan Kölsch',
     brand: 'Highlander Taigh-grùdaidh',
     price: '5',
-    abv: '4.5%'
+    abv: '4.5'
   }
   {
     name: 'Princes of the Universe Porter',
     brand: 'Highlander Taigh-grùdaidh',
     price: '6',
-    abv: '5%'
+    abv: '5'
   }
 
 ];
@@ -49,14 +49,14 @@ function TapList() {
     <div>
       <img id="ouf" width="100%" src={ouf}/>
       {masterTapList.map((beer, index) =>
-        <Beer name={ticket.name}
-          brand={ticket.brand}
-          price={ticket.price} 
-          abv={ticket.abv}
+        <Beer name={beer.name}
+          brand={beer.brand}
+          price={beer.price} 
+          abv={beer.abv}
           key={index}/>
       )}
     </div>
   );
 }
 
-export default TicketList;
+export default TapList;
