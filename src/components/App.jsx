77aa4036b7d16c2202d50b39ab09a4b3a2a10570
megan schulte <megan.schulte.pdx@gmail.com>
2019-07-04 09:10:19 -0700

    add App component

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+import { Switch, Route } from 'react-router-dom';
+
+function App(){
+  return (
+    <div>
+      <Header/>
+     
+    </div>
+  );
+}
+
+export default App;
