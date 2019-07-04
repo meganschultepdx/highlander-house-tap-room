5a1d02cfee2dce709f2198d905ac4b95f2e4bfb5
megan schulte <megan.schulte.pdx@gmail.com>
2019-07-04 11:15:43 -0700

    add employee login comp

---
+++ src/components/EmployeeLogin.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function EmployeeLoginForm(){
+  return (
+    <div>
+      <style jsx>{`
+        #login-form {
+            font-family: sans-serif;
+            width: 300px;
+            border: 2px solid white;
+            background-color: white;
+            border-radius: 8px;
+            z-index: 1;
+            top: 20%;
+            position: absolute;
+            margin-left: 10vw;
+        }
+
+        #form-title {
+            padding-left: 5px;
+        }
+
+        label, input {
+            padding: 5px;
+            margin: 5px;
+            margin-bottom: 10px;
+        }
+
+        input {
+            border: 2px solid lightgrey;
+            background-color: lightgrey;
+            border-radius: 4px;
+        }
+        `}</style>
+      <div id="login-form">
+                <form>
+                    <h1 id="form-title">Employee Login</h1>
+                    <label>Username/email:  </label><br/>
+                    <input
+                        type='text'
+                        id="username" />
+                    <label>Passord:  </label><br/>
+                    <input
+                        type='text'
+                        id="password" />
+                    <button type='submit'>Search</button>
+                </form>
+            </div>
+    </div>
+  );
+}
+
+export default EmployeeLoginForm;
