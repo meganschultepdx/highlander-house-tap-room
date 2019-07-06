34be7780e675a425aefa6a81862932b13e8600d2
megan schulte <megan.schulte.pdx@gmail.com>
2019-07-06 11:53:53 -0700

    styling to info comp

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,13 +9,32 @@ function Title() {
 
       #parent {
         position: relative;
+        margin-top: 40px;
       }
  
       #info {
         z-index: 1;
         position: absolute;
-        top: 18%;
-        left: 21%;
+        top: 5%;
+        left: 5%;
+        color: lightgrey;
+        font-family: sans-serif;
+        border: 1px solid rgb(255, 255, 255, 0);
+        border-radius: 8px;
+        background-color: rgb(255, 255, 255, .1);
+        padding: 30px;
+      }
+
+      .infoTitle {
+        text-transform: uppercase;
+        font-weight: lighter;
+        font-size: 2em;
+      }
+
+      .info {
+        font-weight: lighter;
+        padding-left: 20px;
+        font-size: 1.5em;
       }
 
      
@@ -25,9 +44,14 @@ function Title() {
       <img id="beerPour" width="100%" height="auto" src={guyPouringBeer} alt="guy pouring beer"/>
       <div className="flex-container">
         <div id="info">
-      <h2>Location: </h2>
-      <h2>Hours: </h2>
-      <h2>Phone: </h2>
+      <h2 className="infoTitle">Location: </h2>
+      <h3 className="info">558 Willoway Lane<br />
+      Andersonville, WA 99881</h3>
+      <h2 className="infoTitle">Hours: </h2>
+      <h3 className="info">Thursday - Sunday: 11am - 2am<br />
+      Monday - Wednesday: 4pm - 11pm</h3>
+      <h2 className="infoTitle">Phone: </h2>
+      <h3 className="info">(999)881-beer</h3>
       </div>
       </div>
      </div>
